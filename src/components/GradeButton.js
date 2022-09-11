c16e748f47645ed1f7c6dc16d64caf096bfa1905
chrisdrifte <chrisdrifte@gmail.com>
2022-09-11 09:21:58 +0200

    Improve mobile styles

--- src/components/GradeButton.js
+++ src/components/GradeButton.js
@@ -1,8 +1,12 @@
 export const GradeButton = ({ text, onClick }) => {
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("button");
+
   const button = document.createElement("button");
-  button.classList.add("button");
   button.innerText = text;
   button.addEventListener("click", onClick);
 
-  return button;
+  wrapper.append(button);
+
+  return wrapper;
 };
